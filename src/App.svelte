a8bd6c731434996c327f17ea44f62a0d4a241f51
ru-museum <museum_ru@yahoo.co.jp>
2026-04-15 01:14:43 +0900

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,121 +1,50 @@
 <script lang="ts">
-	import logo from './assets/svelte.png';
-	import vite from './assets/vite.svg';
-	import Counter from './lib/Counter.svelte';
 
-	export let name: string;
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from './assets/vite.svg'
+  import Counter from './lib/Counter.svelte'
+
+	let name: string = $state(0)
 	const buttons: string = [
 	     { name: "foo", color: "red"  },
 	     { name: "bar", color: "blue" },
 	     { name: "baz", color: "green"}
 	];
+
 </script>
 
 <div class="container">
 
-<div class="main">
+<h1>svelte-ie11-boilerplate-vite</h1>
+<h2>Vite - Svelte - Typescript</h2>
+<h3>/src/App.svelte</h3>
 
-  <img src={logo} alt="Svelte Logo" />
-  <img src={vite} alt="Vite Logo" />
-
-  <h1>Hello {name}!</h1>
-  <h1>Svelte - Vite<br> Boilerplate</h1>
-  <h3>Typescript - SCSS - IE11</h3>
-
-  <Counter />
-
-</div>
-
-
-<h2 class="left">設定手順</h2>
-
-<ol class="left">
-
- <li><h3>Typescript：</h3>
-	<ul>
-		<li><strong>Typescript</strong> はそのまま使えます。
-	</ul>	 
-	 
-   <h4>動作テスト</h4>
-
+<h2>環境構築</h2>
+<ol>
+  <li><h3>project</h3>
 <pre>
-   <div>
-&lt;script <strong>lang="ts"</strong>&gt;
-
-   const buttons<strong>: string</strong> = [
-     &#123; name: "foo", color: "red"  &#125;,
-     &#123; name: "bar", color: "blue" &#125;,
-     &#123; name: "baz", color: "green"&#125;
-   ];
-
-&lt;/script&gt;
-
-   </div>
-</pre>    
-
-     <div style="text-align:center;">
-         {#each buttons as {name, color}, i}
-
-            <button class="btn" 
-                    style="background-color:{color};">
-               {name}
-            </button>
-
-         {/each}
-     </div>
-   
-    </li>
-   
-   <li><h3>SCSS：</h3>
-   
-   <ul>
-     <li><strong>sass</strong> のインストールのみで使用可となっています。
-   </ul>
-
-   <h4>動作テスト</h4>	   
-	   
-<pre>    
-   <div>
-&lt;style <strong>lang="scss"</strong>&gt;
-
-   //変数の宣言 
-   <strong>$bgcolor</strong>: #e1f5fe;
-   <strong>$width</strong>:400px;
-   <strong>$color</strong>: green;
-
-   .sample &#123;
-      background-color: <strong>$bgcolor</strong>;
-      width:<strong>$width</strong>;
-      .content p &#123;
-         color: <strong>$color</strong>;
-      &#125;
-   &#125;
-   
-&lt;/style&gt;
-   </div>
-   
-</pre>    
-
-   <div class="sample">
-     <h3>SCSS テスト</h3>
-     <div class="content">
-       <p>変数の使用</p>
-     </div>
-   </div> 
-   
+npm create vite@latest project-name
+◯ Svelte  
+◯ Typescript
+</pre>  
 </li>
-
-
-<li>
-   <h3>IE11 対応</h3>
-   <ul>
-     <li><strong>@vitejs/plugin-legacy</strong> をインストールしています。</li>
-   </ul>
-
-   <h4>./vite.config.js</h4>
-	
+  <li><h3>main.ts</h3>
 <pre>
+import './app.css' 
++ import './style.css'
+</pre>  
+  </li>
 
+<li><h3>IE 11: plugin-legacy</h3>
+<ol style="list-style-type: decimal-leading-zero;">
+  <li><h4>インストール</h4>
+<pre>
+# npm i -D @vitejs/plugin-legacy
+</pre>  
+
+</li>
+<li><h4>vite.config.ts</h4>
+<pre class="w600">
 import &#123; defineConfig &#125; from 'vite';
 import &#123; svelte &#125; from '@sveltejs/vite-plugin-svelte';
 
@@ -123,146 +52,191 @@ import &#123; svelte &#125; from '@sveltejs/vite-plugin-svelte';
 <strong>import legacy from '@vitejs/plugin-legacy';</strong>
 
 export default defineConfig(&#123;
-
    plugins: [
-   
-      svelte(),
-
-      <strong>
+      svelte(),<strong>
       // IE11 対応
-     legacy(&#123;
+      legacy(&#123;
         targets: ['ie >= 11'],
         additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-     &#125;)
-      </strong>
-      
+      &#125;)</strong>
    ],
-   
    server: &#123;
       // PORT 変更：必要なければ不要（default：3000）　　
       port: 5000
    &#125;,
-  
    // 表示エラーの出力修正： "/assets/..." => "./assets/..."　
    base : './',
-   
 &#125;)
-
 </pre>
-   <p></p>
+</li>
+</ol>
+
+  </li>
+
+  <li><h3>tsconfig.json</h3>
+<pre class="w600">
+ERROR:
+The TypeScript option <strong>verbatimModuleSyntax</strong> is now required 
+when using Svelte files with lang="ts". Please add it to 
+your tsconfig.json.
+
+// tsconfig.app.json ではなく直接 <strong>tsconfig.json</strong> に記述します。
+&#123;
+  "compilerOptions": &#123;
+    <strong>"verbatimModuleSyntax": true,</strong>
+  &#125;,
+</pre>
+  </li>
+
+  <li><h3>package.json</h3>
+<pre class="w500">
+"@sveltejs/vite-plugin-svelte": "^7.0.0",
+"@tsconfig/svelte": "^5.0.8",
+"@types/node": "^25.6.0",
+"@vitejs/plugin-legacy": "^8.0.1",
+"marked": "^18.0.0",
+"sass": "^1.99.0",
+"svelte": "^5.55.3",
+"svelte-check": "^4.4.6",
+"svelte-preprocess": "^6.0.3",
+"tslib": "^2.8.1",
+"typescript": "^5.9.3",
+"vite": "^8.0.8",
+"whatwg-fetch": "^3.6.20"
+</pre>
+※ typescript は svelte-preprocess 依存のため "5.9.3" です。
+  </li>
+
+ <li><h3>Typescript：</h3>
+	<ul>
+		<li><strong>Typescript</strong> はそのまま使えます。</li>
+	</ul>	 
+	 
+   <h4>動作テスト</h4>
+<pre>
+&lt;script <strong>lang="ts"</strong>&gt;
+const buttons<strong>: string</strong> = [
+ &#123; name: "foo", color: "red"  &#125;,
+ &#123; name: "bar", color: "blue" &#125;,
+ &#123; name: "baz", color: "green"&#125;
+];
+&lt;/script&gt;
+</pre>    
+
+{#each buttons as {name, color}, i}
+  <button class="btn" style="background-color:{color};">
+   {name}
+  </button>
+{/each}
+ <br>
+ <br>
+  <Counter />
+
+  </li>
    
+   <li><h3>SCSS</h3>
+   
+   <ol style="list-style-type: decimal-leading-zero;">
+     <li><h4>インストール：</h4>
+<pre class="w300">
+npm i -D sass
+</pre>
 </li>
 
-<li>
-   <h3>エラー対応</h3>
-   <ul>
-      <li>./dist/index.html を開いても表示されない場合、ディレクトリ指定を「./」とする必要があります。<br>
-      ※ ブラウザ依存であるかは未確定（環境：Firefox - Debian）。
-      </li>
-      <li>
-         build 時に "/assets/" が "<strong>.</strong>/assets/" となる様に指定します。
-      </li>
-   </ul>
-
-   <h4>./vite.config.js</h4>
-	
+<li><h4>CSS</h4>
 <pre>
-export default defineConfig(&#123;
-   ....
-   
-   <strong>base : './'</strong>
+&lt;style <strong>lang="scss"</strong>&gt;
+   /* 変数の宣言 */
+   <strong>$bgcolor</strong>: #e1f5fe;
+   <strong>$width</strong>:400px;
+   <strong>$color</strong>: green;
+   .sample &#123;
+      background-color: <strong>$bgcolor</strong>;
+      width:<strong>$width</strong>;
+      padding:20px 20px;
+      p &#123;
+         color: <strong>$color</strong>;
+      &#125;
+   &#125;
+&lt;/style&gt;
+</pre>    
+</li>
+<li><h4>動作テスト</h4>	   
+
+<pre>
+&lt;div class="sample"&gt;
+  &lt;p&gt;変数の使用&lt;/p&gt;
+&lt;/div&gt; 
 </pre>
+
+
+   <div class="sample">
+       <p>変数の使用</p>
+   </div> 
+</li>
+</ol>
+<br>   
+</li>
+
+<li><h2>LINKS</h2>
+   <ol style="list-style-type: decimal-leading-zero;">
+    <li>
+      <a href="https://vite.dev/" target="_blank" rel="noreferrer">
+        <img class="logo" src={viteLogo} alt="viteLogo" />
+        Vite
+      </a>
+    </li>
+    <li>
+      <a href="https://svelte.dev/" target="_blank" rel="noreferrer">
+        <img class="button-icon" src={svelteLogo} alt="svelteLogo" />
+        Svelte
+      </a>
+    </li>
+  </ol>
+ 
 </li>
 
 </ol>
 
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> and <a href="https://ja.vitejs.dev/" target="_blank">Vitejs</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
-  
 </div>
+
+<br>
+<br>
 
 <style lang="scss">
 
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
+   /* 変数の宣言*/ 
+   $bgcolor: #e1f5fe;
+   $width: 200px;
+   $color: green;
 
-	.left {
-		text-align: left;
-	}
-
-	.main{
-		text-align: center;
-	}
-
-	.container {
-		padding-top: 4em;
-		width: 700px;
-		margin: 0 auto;
-	}
-
-	img {
-		width: 40px;
-		height: 40px;
-	}
-
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 3em;
-		font-weight: 100;
-	}
-
-	//変数の宣言 
-	$bgcolor: #e1f5fe;  
-	$width: 400px;
-	$color: green;
-
-	pre {
-		background-color: #f4f9ff;
-		padding: 12px;
-	}
-
-	.sample {
-		background-color: $bgcolor;
-		width:$width;
-		margin: 0 auto;
-		padding: 14px 24px;
-
-		h3 {
-			 margin: 20px;
-			 font-size: 24px;
-		}
-		.content p {
-			 color: $color;
-		}
-	}
-
-	strong {
-		color: $color;
-	}
+   .sample {
+      font-size:1.4em;
+      text-align: center;
+      background-color: $bgcolor;
+      width:$width;
+      p {
+         color: $color;
+      }
+      padding:26px 20px 26px 20px;
+   }
 
 	.btn{
 		margin-right: 20px;
+    font-size:3em;
 		color:#fff;
-		padding:16px;
+		padding:1.2em;
 		border:0;
 		border-radius:12px;
 	}
 
+  .logo {
+    height: 18px;
+  }
 
-	@media (min-width: 640px) {
-		.main {
-			max-width: none;
-		}
-	}
+  .button-icon {
+    height: 18px;
+    width: 18px;
+  }
+
 </style>
